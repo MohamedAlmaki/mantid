e550d9d5634469ee713460ade6b78481b9d835fe
Mohammed Almakki <mohammed.almakki@stfc.ac.uk>
2026-06-17 17:26:40 +0100

    Add failed test

--- qt/icons/test/IconTest.h
+++ qt/icons/test/IconTest.h
@@ -56,6 +56,6 @@ public:
   }
 
   void testIntentionalFailureForWorkflowAnnotationTest() {
-    TS_FAIL("Intentional failure to test GitHub Actions source annotations");
+    TS_FAIL("Intentional failure to test GitHub Actions annotations");
   }
 };
